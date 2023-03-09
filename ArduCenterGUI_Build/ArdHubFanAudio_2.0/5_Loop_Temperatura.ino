9a671d4a939de10d31dd0030ba024d543bd92c7b
MastrMarco <marcomastrangelo33@gmail.com>
2023-03-09 18:08:03 +0100

    Build

--- ArduCenterGUI_Build/ArdHubFanAudio_2.0/5_Loop_Temperatura.ino
+++
@@ -1,9 +0,0 @@
-void TemperaturaDS() {
-
-  if (TimerVirtuale[1] >= (ResetTimerVirtuale[3] + DelayVirtuale[3])) {
-    ResetTimerVirtuale[3] = TimerVirtuale[1];
-
-    if (TempData.readTemp()) TempDS = TempData.getTemp();
-    TempData.requestTemp();
-  }
-}
--- ArduCenterGUI_Build/ArduCenter/F_Audio.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class F_Audio
-
-End Class
